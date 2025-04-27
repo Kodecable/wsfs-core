8736ed7549b3ef097b6a0c9876f5ef534e4a8f45
Kodecable <Kodecable@outlook.com>
2025-04-28 00:32:45 +0800

    feat: hash command option 'cost'

--- src/internal/cmd/hash/hash.go
+++ src/internal/cmd/hash/hash.go
@@ -9,17 +9,25 @@ import (
 	"golang.org/x/term"
 )
 
+var (
+	hashCost uint8
+)
+
 var HashCmd = &cobra.Command{
 	Use:   "hash [Password]...",
 	Short: "Generate bcrypt hash(s) of the given password",
+	Long: `Generate bcrypt hash(s) of the given password
+If no password is given as arguments, one will be read from stdin`,
 	Run: func(_ *cobra.Command, args []string) {
 		if len(args) == 0 {
+			fmt.Printf("Password: ")
 			password, err := term.ReadPassword(int(os.Stdin.Fd()))
+			fmt.Printf("\n")
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Uable to read stdin:", err)
 				os.Exit(1)
 			}
-			hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+			hash, err := bcrypt.GenerateFromPassword([]byte(password), int(hashCost))
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Generate fail:", err)
 				os.Exit(1)
@@ -30,7 +38,7 @@ var HashCmd = &cobra.Command{
 		}
 
 		for _, pw := range args {
-			hash, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.DefaultCost)
+			hash, err := bcrypt.GenerateFromPassword([]byte(pw), int(hashCost))
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Generate fail:", pw, " err:", err)
 			} else {
@@ -38,4 +46,8 @@ var HashCmd = &cobra.Command{
 			}
 		}
 	},
+}
+
+func init() {
+	HashCmd.Flags().Uint8VarP(&hashCost, "cost", "c", uint8(bcrypt.DefaultCost), "Bcrypt hash cost")
 }
